5657ec2b5d28802cfd7c945710e0ce3d04251e75
Mészáros Gergely <goteguru@gmail.com>
2018-12-20 15:53:00 +0100

    statikus kliens

--- tic-tac-toe_server.py
+++ tic-tac-toe_server.py
@@ -1,4 +1,4 @@
-from bottle import get, post, run, request, response
+from bottle import get, post, run, request, response, static_file
 import json
 game=list("_________")
 endgame=[(0,1,2), (3,4,5), (6,7,8), (0,4,8), (2,4,6)]
@@ -46,5 +46,9 @@ def newgame():
 @get("/game")
 def return_game():
     return json.dumps({"error": 0, "game": "".join(game)})
-   
-run(host='192.168.111.183', port=1111, debug=True)
+
+@get('/client')
+def client():
+    return static_file("tic-tac-toe_client.html", ".")
+
+run(host='', port=1111, debug=True)
